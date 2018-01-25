3f8e508996b76b784f9ad730744dd9c4d5ca4720
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 23:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -39,3 +39,8 @@ function getBranches (el) {
   req.send()
 }
 
+function displayBranches() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
