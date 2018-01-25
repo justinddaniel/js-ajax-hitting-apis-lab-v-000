b11f44a4fe513bc32d57981b23aaa98b06f9d5e7
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 23:00:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories () {
-  var username = document.getElementById("username").innerHTML;
+  var username = document.getElementById("username").value;
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories); //when the event fires showRepositories() gets called.
   req.open("GET", `https://api.github.com/users/${username}/repos`)
