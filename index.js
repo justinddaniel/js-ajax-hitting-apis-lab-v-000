7b9fe0989db17c2d6b75b2003f1924805c293dab
Justin Daniel <justinddaniel@gmail.com>
2018-01-25 22:52:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getRepositories () {
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories); //when the event fires showRepositories() gets called.
-  req.open("GET", 'https://api.github.com/users/justinddaniel/repos')
+  req.open("GET", 'https://api.github.com/users/:username/repos')
   req.send()
 }
